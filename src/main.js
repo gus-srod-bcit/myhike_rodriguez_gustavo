37ab106d992e2d13fbc55ea6c80065a58f5c6fdb
gus-srod-bcit <gustavosrod1121@gmail.com>
2025-10-28 19:06:32 -0700

    :fix: everything broke

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import { onAuthReady } from "./authentication.js"
+import { db } from "./firebaseConfig.js";
+import { doc, onSnapshot, getDoc } from "firebase/firestore";
+
+function showDashboard()
+{
+    const nameElement = document.getElementById("name-goes-here"); // the <h1> element to display "Hello, {name}"
+
+    onAuthReady(async (user) => {
+        if (!user)
+        {
+            // If no user is signed in → redirect back to login page.
+            location.href = "index.html";
+            return;
+        }
+
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        const name = userDoc.exists() ? userDoc.data().name : user.displayName || user.email;
+
+        // Update the welcome message with their name/email.
+        if (nameElement)
+        {
+            nameElement.textContent = `${name}!`;
+        }
+    });
+}
+
+function readQuote(day)
+{
+    const quoteDocRef = doc(db, "quotes", day); // Get a reference to the document
+
+    onSnapshot(quoteDocRef, (docSnap) => { // Listen for real-time updates
+        if (docSnap.exists())
+        {
+            document.getElementById("quote-goes-here").innerHTML = docSnap.data().quote;
+        } 
+        else
+        {
+            console.log("No such document!");
+        }
+    }, (error) => {
+        console.error("Error listening to document: ", error);
+    });
+}
+
+showDashboard();
+readQuote("tuesday");
